ba5bb826b9f124ad172a6e708cdf9195402ce4ab
yusufm423 <89308036+yusufm423@users.noreply.github.com>
2021-10-20 17:38:44 +0530

    phase5

--- src/components/Account_page.js
+++ src/components/Account_page.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Account_page from './Account'
+import Accountpage from './Account'
 import Notices from './Notices';
 import Menu from './Menu';
 
@@ -8,7 +8,7 @@ export default function Account() {
         <>
            <div className="row">
             <div className="col-3 my-3 mb-3"><Notices/></div>
-            <div className="col-7 my-3 mb-3"><Account_page/> </div>
+            <div className="col-7 my-3 mb-3"><Accountpage/> </div>
             <div className="col-2 my-3 mb-3"><Menu/></div> 
           </div>  
         </>
