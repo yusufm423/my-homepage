d9b38b09079255cb4f576065c3ab47f243be6596
navedkhan76 <navedkhanmohd76@gmail.com>
2021-11-08 11:14:40 +0530

    Modified Time Table with Responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,9 +7,11 @@ export default function Home() {
     return (
         <>
            <div className="row">
-            <div className="col-3 my-3 mb-3"><Notices/></div>
-            <div className="col-7 my-3 mb-3"><HomeMid/> </div>
-            <div className="col-2 my-3 mb-3"><Menu/></div> 
+           <div className="col-7 my-3 mb-3 Homemid1"><HomeMid /> </div>
+           <div className="col-2 my-3 mb-3 menu1"><Menu /></div>
+            <div className="col-3 my-3 mb-3"><Notices /></div>
+            <div className="col-7 my-3 mb-3 Homemid2"><HomeMid /> </div>
+            <div className="col-2 my-3 mb-3 menu2"><Menu /></div> 
           </div>  
         </>
     )
